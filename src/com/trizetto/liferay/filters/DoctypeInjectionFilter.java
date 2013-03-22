daa20438e2944c800b362a571e036fe8e4057645
yuriy lazebnyk <ylazebnik@gmail.com>
2013-03-22 15:25:13 +0200

    Initial commit. Add DoctypeInjectionFilter class. It is adding doctype declaration to response stream before passing context to the next filter

---
+++ src/com/trizetto/liferay/filters/DoctypeInjectionFilter.java
@@ -0,0 +1,30 @@
+package com.trizetto.liferay.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * User: Yuriy.Lazebnik@trizetto.com
+ * Date: 3/22/13
+ * Time: 1:28 PM
+ */
+public class DoctypeInjectionFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        response.getWriter().write("<!DOCTYPE html>");
+        chain.doFilter(request, response);
+    }
+}
